1df6100923e7cedf15ec7749ae4ed40c2b53cd4b
maykon-4 <maykon.abreu@escola.pr.gov.br>
2025-08-19 14:12:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'educação fisica',
+    'qualesporte e jogado na qudra  com chuteira?',
+    'esprte futsal.'
+)
+
+criaCartao(
+    'Geografia',
+    'o q estuda na geografia? ',
+    'os espaço geografico.'
+)
+
+criaCartao(
+    'projeto de vida ',
+    'qual a principal tema?',
+    'é o autoconhecimento.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz escola?',
+    'A palavra "escola" em inglês é "school".'
+)
